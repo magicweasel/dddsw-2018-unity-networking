5beac55ca1d9e752d2ac9f5565e4ddfaf0b28673
PMDOMUK\andrew.clarke <andrew.clarke@protolabs.co.uk>
2018-04-22 11:35:58 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# dddsw-2018-unity-networking
+# dddsw-2018
+
+## Unity 101 for C# Developers - Andy Clarke
+
+## Unity Networking
 A cleaned up version of the Unity networking code I used in my session at DDDSW.
+
+**Getting Started**
+I've included built binaries, so if you just want to have look at the exe running
+on windows you can use that.
+
+For playing around with the Source you need a copy of Unity and Visual Studio
+(or your favourite C# editor).
+https://unity3d.com/
+
+You'll want to open the scene in Unity - "Source/Assets/Networking.unity"
+
+Enjoy :)
---
+++ source/Assets/Player.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+	private Animator animator;
+	private Transform grabPoint;
+
+	void Start()
+	{
+		//Disable the camera if it's not local
+		if (!this.isLocalPlayer)
+		{
+			this.GetComponentInChildren<Camera>().enabled = false;
+		}
+
+		this.animator = this.GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		//Don't run the update code if we're not local
+		if (!this.isLocalPlayer)
+		{
+			return;
+		}
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		this.transform.Rotate(0, x, 0);
+		this.transform.Translate(0, 0, z);
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		this.grabPoint = col.transform.GetChild(0);
+		col.transform.parent = this.transform;
+	}
+
+	void OnAnimatorIK()
+	{
+		if (this.grabPoint != null)
+		{
+			this.animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+			this.animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+			this.animator.SetIKPosition(AvatarIKGoal.RightHand, this.grabPoint.position);
+			this.animator.SetIKRotation(AvatarIKGoal.RightHand, this.grabPoint.rotation);
+		}
+		else
+		{
+			this.animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+			this.animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+			this.animator.SetLookAtWeight(0);
+		}
+	}
+}
